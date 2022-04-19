d60c406f08a83c9c7aba68c82ade49e82b17f9b4
romanboyarkin <boyra75.rb@gmail.com>
2022-04-20 01:02:38 +0900

    step 2/6

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -13,4 +13,19 @@ Coffee is ready!
         
     """
     )
+    howManyCups()
+}
+
+fun howManyCups() {
+    val water = 200
+    val milk = 50
+    val beans = 15
+    println("Write how many cups of coffee you wil need")
+    val cups = readLine()!!.toInt()
+    println("For $cups cups of coffee you will need:")
+    println("${water * cups} ml of water")
+    println("${milk * cups} ml of milk")
+    println("${beans * cups} g of coffee beans")
+
+
 }

6a2341b5c116b3f64cd1cb0469b5294edc42a3a1
romanboyarkin <boyra75.rb@gmail.com>
2022-04-24 01:59:24 +0900

    When expression3

--- Topics/When expression/The army of units/src/Main.kt
+++ Topics/When expression/The army of units/src/Main.kt
@@ -1,16 +1,17 @@
 fun main() {
     // write your code here
     val n = readLine()!!.toInt()
-    when(n) {
-        1 -> println("no army")
-        in 1..4 -> println("few")
-        in 5..9 -> println("several")
-        in 10..19 -> println("pack")
-        in 20..49 -> println("lots")
-        in 50..99 -> println("horde")
-        in 100..249 -> println("throng")
-        in 250..499 -> println("swarm")
-        in 500..999 -> println("zounds")
-        else -> println("legion")
+    val res = when(n) {
+        1 -> "no army"
+        in 1..4 -> "few"
+        in 5..9 -> "several"
+        in 10..19 -> "pack"
+        in 20..49 -> "lots"
+        in 50..99 -> "horde"
+        in 100..249 -> "throng"
+        in 250..499 -> "swarm"
+        in 500..999 -> "zounds"
+        else -> "legion"
     }
+    println(res)
 }

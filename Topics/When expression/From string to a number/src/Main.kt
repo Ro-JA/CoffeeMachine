4300ae3aa0adf9f68809e3383b371f2bb731f82e
romanboyarkin <boyra75.rb@gmail.com>
2022-04-25 20:44:35 +0900

    correct3

--- Topics/When expression/From string to a number/src/Main.kt
+++ Topics/When expression/From string to a number/src/Main.kt
@@ -5,9 +5,20 @@ const val FOUR = "four"
 const val FIVE = "five"
 const val SIX = "six"
 const val SEVEN = "seven"
-const val
+const val EIGHT = "eight"
+const val NINE = "nine"
+
 fun main() = print(
     when (readLine()!!) {
-
+        ONE -> 1
+        TWO -> 2
+        THREE -> 3
+        FOUR -> 4
+        FIVE -> 5
+        SIX -> 6
+        SEVEN -> 7
+        EIGHT -> 8
+        NINE -> 9
+        else -> "error"
     }
 )

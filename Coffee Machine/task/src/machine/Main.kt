a377a12ef9828aaf3b06c45fdc0f78f211fb991c
romanboyarkin <boyra75.rb@gmail.com>
2022-04-27 23:12:02 +0900

    add fun startCoffeeMachine

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -3,23 +3,27 @@ package machine
 const val WATER_CUP_ESPRESSO = 250
 const val WATER_CUP_LATTE = 350
 const val WATER_CUP_CAPPUCCINO = 200
+const val WATER_CUP = 200
 const val MILK_CUP_LATTE = 75
 const val MILK_CUP_CAPPUCCINO = 100
+const val MILK_CUP = 50
 const val BEANS_CUP_ESPRESSO = 16
 const val BEANS_CUP_LATTE= 20
 const val BEANS_CUP_CAPPUCCINO = 12
+const val BEANS_CUP = 15
 const val PRICE_ESPRESSO = 4
 const val PRICE_LATTE = 7
 const val PRICE_CAPPUCCINO = 6
+const val WATER = 400
+const val MILK = 540
+const val BEANS = 120
+const val MONEY = 550
+const val CUPS = 9
 
 
 fun main() {
 
-    ingredientCalculatorCoffee()
-
-    numberOfServings()
-
-    coffeeProcessText()
+   startCoffeeMachine()
 }
 
 fun coffeeProcessText() {
@@ -50,11 +54,11 @@ fun ingredientCalculatorCoffee() {
 
 fun numberOfServings() {
     println("Write how many ml of water the coffee machine has:")
-    val waterUser = readLine()!!.toInt() / WATER_CUP
+    val waterUser = readLine()!!.toInt()
     println("Write how many ml of milk the coffee machine has:")
-    val milkUser = readLine()!!.toInt() / MILK_CUP
+    val milkUser = readLine()!!.toInt()
     println("Write how many grams of coffee beans the coffee machine has:")
-    val beansUser = readLine()!!.toInt() / BEANS_CUP
+    val beansUser = readLine()!!.toInt()
     println("Write how many cups of coffee you will need:")
     val cupsUser = readLine()!!.toInt()
     val cupsInMachine = listOf<Int>(waterUser, milkUser, beansUser).minOrNull()
@@ -66,4 +70,17 @@ fun numberOfServings() {
         println("Yes, I can make that amount of coffee (and even ${cupsInMachine - cupsUser} more than that)")
     }
 
+}
+
+fun startCoffeeMachine() {
+    println("""
+        The coffee machine has:
+        $WATER ml of water
+        $MILK ml of milk
+        $BEANS g of coffee beans
+        $CUPS disposable cups
+        ${'$'}$MONEY of money
+
+        Write action (buy, fill, take): 
+    """.trimIndent())
 }

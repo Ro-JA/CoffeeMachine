5a72990a3175413cb12ffd10298cede1904ff7c1
romanboyarkin <boyra75.rb@gmail.com>
2022-05-08 19:10:26 +0900

    Correct

--- Topics/Functional decomposition/Calculator with four functions/src/Main.kt
+++ Topics/Functional decomposition/Calculator with four functions/src/Main.kt
@@ -1,20 +1,12 @@
 // Implement your functions here
 
-fun subtractTwoNumbers(a: Long, b: Long) = a - b
+fun subtractTwoNumbers(a: Long, b: Long) = println(a - b)
 
 
-fun sumTwoNumbers(a: Long, b: Long) = a + b
+fun sumTwoNumbers(a: Long, b: Long) = println(a + b)
 
 
-fun divideTwoNumbers(a: Long, b: Long): Long {
-    var result= 0L
-    if (b.toInt() == 0) {
-        println("Division by 0!")
-    } else {
-        result = (a / b)
-    }
-    return result
-}
+fun divideTwoNumbers(a: Long, b: Long) = println(if (b.toInt() == 0) "Division by 0!" else a / b)
 
 
-fun multiplyTwoNumbers(a: Long, b: Long) = a * b
+fun multiplyTwoNumbers(a: Long, b: Long) = println(a * b)

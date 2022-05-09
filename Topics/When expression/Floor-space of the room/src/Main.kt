242c6ea27af1168066794d5d39f2bbcddebcc2d1
romanboyarkin <boyra75.rb@gmail.com>
2022-05-10 00:58:03 +0900

    Correct

--- Topics/When expression/Floor-space of the room/src/Main.kt
+++ Topics/When expression/Floor-space of the room/src/Main.kt
@@ -1,21 +1,33 @@
 fun main() {
     val input = readln()
-    val a = readln().toDouble()
-    val b = readln().toDouble()
-    val c = readln().toDouble()
 
     when(input) {
-        "triangle" -> funTriangle(a, b, c)
-        "rectangle" -> funRectangle(a, b)
-        "circle" -> funCircle(pi = 3.1415926535, a)
+        "triangle" -> funTriangle()
+        "rectangle" -> funRectangle()
+        "circle" -> funCircle()
     }
 }
 
-fun funCircle(pi: Double = 3.1415926535, a: Double ) = pi * a * a
+fun funCircle() {
+    val pi = 3.14
+    val a = readln().toDouble()
+    println(pi * a * a)
+}
 
-fun funRectangle(a: Double, b: Double) = a * b
+fun funRectangle() {
+    val a = readln().toDouble()
+    val b = readln().toDouble()
+    println(a * b)
+}
 
-fun funTriangle(a: Double, b: Double, c: Double) = ((a + b + c) / 2 * ((a + b + c) / 2 - a) * ((a + b + c) / 2 - b)
-    * ((a + b + c) / 2 - c))
+fun funTriangle() {
+    val a = readln().toDouble()
+    val b = readln().toDouble()
+    val c = readln().toDouble()
+    println(
+        kotlin.math.sqrt((a + b + c) / 2 * ((a + b + c) / 2 - a) * ((a + b + c) / 2 - b)
+                * ((a + b + c) / 2 - c))
+    )
+}
 
 

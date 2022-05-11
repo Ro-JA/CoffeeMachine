bbe8da6631be92bdf177537e911f088f0e8d2f87
romanboyarkin <boyra75.rb@gmail.com>
2022-05-11 23:37:47 +0900

    refactoring

--- Topics/Default arguments/Gratuity/src/Main.kt
+++ Topics/Default arguments/Gratuity/src/Main.kt
@@ -1,4 +1,1 @@
-fun tip(bill: Int, percentage: Int = 10 ): Int {
-   val gratuity = bill * percentage / 100
-   return gratuity
-}
+fun tip(bill: Int, percentage: Int = 10 ): Int = bill * percentage / 100

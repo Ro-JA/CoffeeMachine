bbfa18e9c9d08f4fae52dfa1e0ebab75e4cb6adf
romanboyarkin <boyra75.rb@gmail.com>
2022-05-11 23:48:41 +0900

    Correct

--- Topics/Default arguments/Gratuity/src/Main.kt
+++ Topics/Default arguments/Gratuity/src/Main.kt
@@ -1,1 +1,3 @@
-fun tip(bill: Int, percentage: Int = 10 ): Int = bill * percentage / 100
+const val TEEN = 10
+const val HANDRED = 100
+fun tip(bill: Int, percentage: Int = TEEN): Int = bill * percentage / HANDRED

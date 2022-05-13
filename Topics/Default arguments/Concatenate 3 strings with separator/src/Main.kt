01ba13d9191177a6764fd7e80eb7c83849fcf2a4
romanboyarkin <boyra75.rb@gmail.com>
2022-05-13 23:52:26 +0900

    Correct

--- Topics/Default arguments/Concatenate 3 strings with separator/src/Main.kt
+++ Topics/Default arguments/Concatenate 3 strings with separator/src/Main.kt
@@ -1,2 +1,2 @@
 // complete this function
-fun concatenate(a: String, b: String, c: String, d: String = " " ) = "$a$d$b$d$c"
+fun concatenate(a: String, b: String, c: String, d: String = " ") = "$a$d$b$d$c"

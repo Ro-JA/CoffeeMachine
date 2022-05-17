d34256aa24fef30881e4cff13398cee22be2ea1d
romanboyarkin <boyra75.rb@gmail.com>
2022-05-17 21:53:31 +0900

    Correct

---
+++ Topics/Named arguments/Car speed/src/Main.kt
@@ -0,0 +1,8 @@
+const val N = 60
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    val n1 = readLine()!!
+    if (n1 == "no limit") {
+        println("Exceeds the limit by ")
+    }
+}
--- Topics/When expression/Strings as objects/src/Main.kt
+++ Topics/When expression/Strings as objects/src/Main.kt
@@ -1,12 +1,1 @@
-fun main() {
-    val input = readLine()!!
-    if (input.isEmpty()) {
-        println(input)
-    } else {
-        when (input.first()) {
-            'i' -> println(input.drop(1).toInt() + 1)
-            's' -> println(input.drop(1).reversed())
-            else -> println(input)
-        }
-    }
-}
+fun main()= println

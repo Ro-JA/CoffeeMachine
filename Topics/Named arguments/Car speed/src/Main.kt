d10bb86a860e9f06869205915eaec27abce256d0
romanboyarkin <boyra75.rb@gmail.com>
2022-05-17 23:28:29 +0900

    Correct

--- Topics/Named arguments/Car speed/src/Main.kt
+++ Topics/Named arguments/Car speed/src/Main.kt
@@ -3,6 +3,16 @@ fun main(args: Array<String>) {
     val n = readLine()!!.toInt()
     val n1 = readLine()!!
     if (n1 == "no limit") {
-        println("Exceeds the limit by ")
+        speedCheek(n, n1 = N)
+    } else {
+        speedCheek(n, n1.toInt())
+    }
+}
+
+fun speedCheek(n: Int,n1: Int) {
+    if (n <= n1) {
+        println("Within the limit")
+    } else {
+        println("Exceeds the limit by ${n - n1} kilometers per hour")
     }
 }

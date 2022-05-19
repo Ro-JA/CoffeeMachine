10bedd416dd676e1ef8aa3461434d68f8c168513
romanboyarkin <boyra75.rb@gmail.com>
2022-05-19 23:49:01 +0900

    tomorrow

--- Topics/Named arguments/Car price/src/Main.kt
+++ Topics/Named arguments/Car price/src/Main.kt
@@ -1,4 +1,5 @@
 // complete this function, add default values
-fun carPrice(old: Int, kilometers, maximumSpeed: Int, automatic: Boolean){
+fun carPrice(old: Int = 5, kilometers: Int = 100000, maximumSpeed: Int = 120, automatic: Boolean = false) {
+
 
 }

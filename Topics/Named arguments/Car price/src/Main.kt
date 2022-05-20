25a0dffccb7f9d2b7487fdbf9ede3c9d8dea37ae
romanboyarkin <boyra75.rb@gmail.com>
2022-05-20 10:07:39 +0900

    later

--- Topics/Named arguments/Car price/src/Main.kt
+++ Topics/Named arguments/Car price/src/Main.kt
@@ -1,5 +1,10 @@
 // complete this function, add default values
 fun carPrice(old: Int = 5, kilometers: Int = 100000, maximumSpeed: Int = 120, automatic: Boolean = false) {
+    var price = 8000
+    if(old != 5) {
+        price -=(old - 5) * 200
+    }
+    if (kilometers != 100000) {
 
-
+    }
 }

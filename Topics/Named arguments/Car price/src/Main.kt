5c48c9d80931f64d81159439af82cc46d43f05af
romanboyarkin <boyra75.rb@gmail.com>
2022-05-21 21:43:11 +0900

    incorrect

--- Topics/Named arguments/Car price/src/Main.kt
+++ Topics/Named arguments/Car price/src/Main.kt
@@ -5,6 +5,13 @@ fun carPrice(old: Int = 5, kilometers: Int = 100000, maximumSpeed: Int = 120, au
         price -=(old - 5) * 200
     }
     if (kilometers != 100000) {
-
+        price += 100000 - kilometers / 10000 * 200
     }
+    if (maximumSpeed != 120) {
+        price -= 120 - maximumSpeed * 100
+    }
+    if (automatic) {
+        price += 1500
+    }
+    println(price)
 }

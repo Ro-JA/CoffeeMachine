723c989aa728669493f9e2c507435b5b972e5d98
romanboyarkin <boyra75.rb@gmail.com>
2022-06-01 23:41:52 +0900

    Correct

--- Topics/When expression/A simple calculator/src/Main.kt
+++ Topics/When expression/A simple calculator/src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    // write your code here
+    val (var1, op, var2) = readln().split(" ")
+    val a = var1.toLong()
+    val b = var2.toLong()
+    if (b > 0) {
+        when (op) {
+            "+" -> println(a + b)
+            "-" -> println(a - b)
+            "/" -> println(a / b)
+            "*" -> println(a * b)
+            else -> println("Unknown operator")
+        }
+    } else {
+        println("Division by 0!")
+    }
 }

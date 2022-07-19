88c640eb607d34c99a75f3a43683e806f8873564
romanboyarkin <boyra75.rb@gmail.com>
2022-07-19 12:31:18 +0900

    incorrect

---
+++ Topics/Declaring classes/New planet/src/Main.kt
@@ -0,0 +1,1 @@
+class Planet(val start: String = "Unknown Star",val mass: Double = 1.0, val numberOfSatellites: Int = 1)
--- Topics/Repeating blocks/Repeat by value/src/Main.kt
+++ Topics/Repeating blocks/Repeat by value/src/Main.kt
@@ -1,4 +1,4 @@
 fun main() {
-    // write your code here
-
+    val input = readln().toInt()
+    repeat(input) { print(input) }
 }

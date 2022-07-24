a1dda29017e0b4757d3f97444ebdd204f79acd1d
romanboyarkin <boyra75.rb@gmail.com>
2022-07-25 00:05:05 +0900

    correct

--- Topics/Ranges/At least one/src/Main.kt
+++ Topics/Ranges/At least one/src/Main.kt
@@ -1,3 +1,8 @@
-new fun main() {
-    // write your code here
+fun main() {
+    val inputOne = readLine()!!.toInt()
+    val inputTwo = readLine()!!.toInt()
+    val inputTree = readLine()!!.toInt()
+    val inputFour = readLine()!!.toInt()
+    val inputFive = readLine()!!.toInt()
+    println(inputFive in inputOne..inputTwo|| inputFive in inputTree..inputFour)
 }
